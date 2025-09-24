a95e2342153b3aa48c63cdf8ca370fc3d6982706
Liang Wu <wu.liang.amsterdam@gmail.com>
2025-09-24 15:41:23 +0200

    Update superset_init.sh

--- config/superset_init.sh
+++ config/superset_init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# create Admin user, you can read these values from env or anywhere else possible
-superset fab create-admin --username "$ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$ADMIN_EMAIL" --password "$ADMIN_PASSWORD"
-
 # Upgrading Superset metastore
 superset db upgrade
 
