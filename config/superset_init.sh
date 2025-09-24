5c962a44273fda04a572718e4ec69c125216f95f
Liang Wu <wu.liang.amsterdam@gmail.com>
2025-09-24 16:34:17 +0200

    Update superset_init.sh

--- config/superset_init.sh
+++ config/superset_init.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+set -e
 
 # Upgrading Superset metastore
 superset db upgrade
 
+# 
+if [ -n "$ADMIN_USERNAME" ] && [ -n "$ADMIN_PASSWORD" ] && [ -n "$ADMIN_EMAIL" ]; then
+  superset fab create-admin \
+    --username "$ADMIN_USERNAME" \
+    --firstname "Admin" \
+    --lastname "User" \
+    --email "$ADMIN_EMAIL" \
+    --password "$ADMIN_PASSWORD" || true
+fi
+
 # setup roles and permissions
-superset superset init 
+superset init 
 
 # Starting server
 /bin/sh -c /usr/bin/run-server.sh
