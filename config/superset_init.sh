ed7c0ce14e12e03619a9186eac9de8119c7eaec6
Liang Wu <wu.liang.amsterdam@gmail.com>
2025-09-25 15:52:56 +0200

    Update superset_init.sh

--- config/superset_init.sh
+++ config/superset_init.sh
@@ -26,4 +26,21 @@ superset init
 
 # --- Start web server (becomes PID 1) ---
 echo "[init] Launching web server on [::]:${PORT:-8088}..."
-exec /usr/bin/run-server.sh
+
+HYPHEN_SYMBOL='-'
+
+exec gunicorn \
+    --bind "[::]:${PORT:-8088}" \
+    --access-logfile "${ACCESS_LOG_FILE:-$HYPHEN_SYMBOL}" \
+    --error-logfile "${ERROR_LOG_FILE:-$HYPHEN_SYMBOL}" \
+    --workers ${SERVER_WORKER_AMOUNT:-1} \
+    --worker-class ${SERVER_WORKER_CLASS:-gthread} \
+    --threads ${SERVER_THREADS_AMOUNT:-20} \
+    --log-level "${GUNICORN_LOGLEVEL:-info}" \
+    --timeout ${GUNICORN_TIMEOUT:-60} \
+    --keep-alive ${GUNICORN_KEEPALIVE:-2} \
+    --max-requests ${WORKER_MAX_REQUESTS:-0} \
+    --max-requests-jitter ${WORKER_MAX_REQUESTS_JITTER:-0} \
+    --limit-request-line ${SERVER_LIMIT_REQUEST_LINE:-0} \
+    --limit-request-field_size ${SERVER_LIMIT_REQUEST_FIELD_SIZE:-0} \
+    "${FLASK_APP}"
